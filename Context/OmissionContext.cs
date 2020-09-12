23c9331d5906f1ebee7cda19591ec82ed42a68bd
Yunus Ünver <yunus.unver@bilgeadam.com>
2020-09-12 16:24:18 +0300

    add migration

--- Context/OmissionContext.cs
+++ Context/OmissionContext.cs
@@ -4,13 +4,18 @@ using omission.api.Models;
 
 namespace omission.api.Context
 {
-    public class OmissionContext:DbContext {
-        
+    public class OmissionContext : DbContext
+    {
+
         private IConfiguration _configuration;
+        public OmissionContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionString = _configuration.GetConnectionString("omissionDB");
-            
+
             optionsBuilder.UseNpgsql(connectionString);
         }
 
---
+++ Migrations/20200912131926_FirstMigration.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace omission.api.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Codes",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    createddate = table.Column<DateTime>(nullable: true),
+                    updateddate = table.Column<DateTime>(nullable: true),
+                    isdeleted = table.Column<bool>(nullable: false),
+                    createdby = table.Column<int>(nullable: false),
+                    updatedby = table.Column<int>(nullable: false),
+                    lookupid = table.Column<int>(nullable: false),
+                    title = table.Column<string>(nullable: true),
+                    description = table.Column<string>(nullable: true),
+                    body = table.Column<string>(nullable: true),
+                    hashtags = table.Column<int[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Codes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hashtags",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    createddate = table.Column<DateTime>(nullable: true),
+                    updateddate = table.Column<DateTime>(nullable: true),
+                    isdeleted = table.Column<bool>(nullable: false),
+                    name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hashtags", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lookups",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    createddate = table.Column<DateTime>(nullable: true),
+                    updateddate = table.Column<DateTime>(nullable: true),
+                    isdeleted = table.Column<bool>(nullable: false),
+                    name = table.Column<string>(nullable: true),
+                    type = table.Column<string>(nullable: true),
+                    parentid = table.Column<int>(nullable: true),
+                    orderid = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lookups", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    createddate = table.Column<DateTime>(nullable: true),
+                    updateddate = table.Column<DateTime>(nullable: true),
+                    isdeleted = table.Column<bool>(nullable: false),
+                    name = table.Column<string>(nullable: true),
+                    surname = table.Column<string>(nullable: true),
+                    email = table.Column<string>(nullable: true),
+                    profileimageurl = table.Column<string>(nullable: true),
+                    password = table.Column<string>(nullable: true),
+                    rightids = table.Column<int[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Codes");
+
+            migrationBuilder.DropTable(
+                name: "Hashtags");
+
+            migrationBuilder.DropTable(
+                name: "Lookups");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
